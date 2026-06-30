288eea995fc666dac6f4df44d83c951a42acd22e
audawatt <audawatt@iu.edu>
2026-06-30 12:58:43 -0400

    generator

--- password_strength.py
+++ password_strength.py
@@ -2,31 +2,42 @@ import string
 from collections import Counter
 import random
 
-option = input(str("Enter 1 for password strength checker and 2 for password generator: "))
-
-characters = string.ascii_letters + string.digits + string.punctuation
-char_dict = dict.fromkeys(characters, 0)
-
 def strength_checker():
     password = input(str("Enter your password: "))
+
     upper = any(c.isupper() for c in password)
     lower = any(c.islower() for c in password)
     digit = any(c.isdigit() for c in password)
     special = any(c in string.punctuation for c in password)
     length = len(password) >= 10
     repetition = all(count <= 2 for count in Counter(password).values())
+
     strong = (upper and lower and digit and special and length and repetition)
-    
-    print(strong)
+
+    return strong
 
 def generator():
-    pass
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    digit = string.digits 
+    special = string.punctuation
 
-if option == "1":
-    strength_checker()
-elif option == "2":
-    generator()
-else:
-    print("Please enter 1 or 2")
+    password = (random.choices(upper, k=2) + random.choices(lower, k=4) + random.choices(digit, k=2) + random.choices(special, k=2))
+
+    random.shuffle(password)
+
+    return ''.join(password)
+    
+while True:
+    option = input("Enter 1 for password strength checker, 2 for password generator, and 3 to quit: ")
+
+    if option == "1":
+        print(strength_checker())
+    elif option == "2":
+        print(generator())
+    elif option == "3":
+        break
+    else:
+        print("Please enter 1, 2, or 3.")
 
 
